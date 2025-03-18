b8aa35944deb720294a7e16f9c0cba0ddf24e685
lc <lcarlosrincon@gmail.com>
2025-03-17 21:19:46 -0500

    add controller adapter

---
+++ src/main/java/com/lc/appgate/exercise1/controller/SubsequenceResponse.java
@@ -0,0 +1,13 @@
+package com.lc.appgate.exercise1.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class SubsequenceResponse {
+
+    private int count;
+}
